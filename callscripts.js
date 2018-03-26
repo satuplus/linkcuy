c3b0ba038f00921f17c2e7cbbf002caccdb31578
satuplus <satuplus@gmail.com>
2018-03-26 19:46:21 +0700

    Update callscripts.js

--- callscripts.js
+++ callscripts.js
@@ -23,7 +23,7 @@ var x=Math.floor((Math.random()*3)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2018/03/how-to-start-trading.html"}
 if(x=="2"){xxx="/2018/03/best-health-insurance-providers.html"}
-if(x=="3"){xxx="/2018/03/what-is-health-care-management.html"}a_to_ve[i].href="https://donlotsob.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="/2018/03/what-is-health-care-management.html"}a_to_ve[i].href="https://linkcuy.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
